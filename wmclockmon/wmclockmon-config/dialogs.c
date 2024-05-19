f87a1b5dd0e6fd7e741cb8abf14d0b15b7c5949e
Julien BLACHE <jblache@debian.org>
2024-05-19 17:24:12 +0100

    wmclockmon: build against GTK+ 2.0.
    Link: https://bugs.debian.org/437442
    Link: https://bugs.debian.org/641335
    Co-developed-by: Doug Torrance <dtorrance@piedmont.edu>

--- wmclockmon/wmclockmon-config/dialogs.c
+++ wmclockmon/wmclockmon-config/dialogs.c
@@ -38,7 +38,6 @@ void ync_dialog(const char *title, const char *text, void *f_yes, void *f_no) {
                        bouton, TRUE, TRUE, 0);
     GTK_WIDGET_SET_FLAGS(GTK_WIDGET(bouton), GTK_CAN_DEFAULT);
     gtk_widget_grab_default(GTK_WIDGET(bouton));
-    gtk_widget_draw_default(GTK_WIDGET(bouton));
     gtk_widget_show(bouton);
 
     bouton = gtk_button_new_with_label("No");
@@ -96,7 +95,6 @@ void ok_dialog(const char *title, const char *text) {
                        bouton, TRUE, TRUE, 0);
     GTK_WIDGET_SET_FLAGS(GTK_WIDGET(bouton), GTK_CAN_DEFAULT);
     gtk_widget_grab_default(GTK_WIDGET(bouton));
-    gtk_widget_draw_default(GTK_WIDGET(bouton));
     gtk_widget_show(bouton);
 
     gtk_container_set_border_width(GTK_CONTAINER
--- wmclockmon/wmclockmon-config/edit.c
+++ wmclockmon/wmclockmon-config/edit.c
@@ -76,7 +76,6 @@ void edit_dialog(const char *title,
                        bouton, TRUE, TRUE, 0);
     GTK_WIDGET_SET_FLAGS(GTK_WIDGET(bouton), GTK_CAN_DEFAULT);
     gtk_widget_grab_default(GTK_WIDGET(bouton));
-    gtk_widget_draw_default(GTK_WIDGET(bouton));
     gtk_widget_show(bouton);
 
     bouton = gtk_button_new_with_label("Cancel");
