nvimtor/dockapps

commit 7f042fb7959f69cfded6c61e098653d086e1d2cd

wmclockmon: check whether memory has been allocated before freeing.
Two strings, command and light_color, were always freed even though they
were only conditionally allocated.  This could cause "munmap_chunk(): invalid
pointer" errors.  We fix this by keeping track of whether they have been
allocated and only freeing if they have.

wmclockmon/wmclockmon-config/actions.c
@@ -19,11 +19,15 @@
 #include "edit.h"
 #include "actions.h"
 
+extern int free_light_color;
+extern int free_command;
 
 void quit_app() {
-    FREE(command);
+    if (free_command)
+        FREE(command);
     FREE(config_file);
-    FREE(light_color);
+    if (free_light_color)
+	FREE(light_color);
     free_alrm(&alarms);
     gtk_main_quit();
 }
@@ -76,6 +80,14 @@ void set_values() {
 
 
 void save_datas() {
+    if (free_command) {
+        FREE(command);
+        free_command = 0;
+    }
+    if (free_light_color) {
+        FREE(light_color);
+        free_light_color = 0;
+    }
     style_dir         = gtk_entry_get_text(GTK_ENTRY(wid_styledir));
     style_name        = gtk_entry_get_text(GTK_ENTRY(wid_stylename));
     light_color       = gtk_entry_get_text(GTK_ENTRY(wid_color));

wmclockmon/wmclockmon-config/tools.c
@@ -17,6 +17,8 @@
 #include <sys/types.h>
 #include "configfile.h"
 
+int    free_light_color;
+int    free_command;
 
 int fexist(const char *filename) {
     FILE *file;
@@ -139,6 +141,7 @@ void load_cfgfile() {
             printf("Unable to open configuration file \"%s\".\n", config_file);
         return;
     }
+    free_light_color = free_command = 0;
     while (! feof(file)) {
         memset(line, 0, MAXSTRLEN + 1);
         fgets(line, MAXSTRLEN, file);
@@ -151,13 +154,15 @@ void load_cfgfile() {
 
         if (strncmp(line, "Backlight", 9) == 0)
             backlight = getbool(value);
-        else if (strncmp(line, "Color", 5) == 0)
+        else if (strncmp(line, "Color", 5) == 0) {
             light_color = xstrdup(value);
-        else if (strncmp(line, "Alarm", 5) == 0)
+            free_light_color = 1;
+        } else if (strncmp(line, "Alarm", 5) == 0)
             alrm_add(&alarms, value);
-        else if (strncmp(line, "Command", 7) == 0)
+        else if (strncmp(line, "Command", 7) == 0) {
             command = xstrdup(value);
-        else if (strncmp(line, "MessageCmd", 10) == 0)
+            free_command = 1;
+        } else if (strncmp(line, "MessageCmd", 10) == 0)
             msgcmd = xstrdup(value);
         else if (strncmp(line, "Blink", 5) == 0)
             switch_authorized = getbool(value);

wmclockmon/wmclockmon-config/variables.h
@@ -22,5 +22,7 @@ char  *light_color;
 char  *command;
 char  *msgcmd;
 Alarm *alarms;
+int    free_light_color;
+int    free_command;
 
 #endif

commit f6f51aa00364c56326e16834cb38e990b2daaa83

wmclockmon: properly fix allocating and freeing of `command`, `light_color` and the other GtkEntry strings
Instead of sometimes allocating memory and sometimes storing the values return
by `gtk_entry_get_text()`, always storing allocated memory and unconditionally
free it.

Do this for all the entry strings.

Fixes: 7f042fb7959f ("wmclockmon: check whether memory has been allocated before freeing.")
Signed-off-by: Jeremy Sowden <azazel@debian.org>

wmclockmon/wmclockmon-config/actions.c
@@ -18,15 +18,13 @@
 #include "edit.h"
 #include "actions.h"
 
-extern int free_light_color;
-extern int free_command;
-
 void quit_app() {
-    if (free_command)
-        FREE(command);
+    FREE(command);
     FREE(config_file);
-    if (free_light_color)
-	FREE(light_color);
+    FREE(light_color);
+    FREE(msgcmd);
+    FREE(style_dir);
+    FREE(style_name);
     free_alrm(&alarms);
     gtk_main_quit();
 }
@@ -79,19 +77,17 @@ void set_values() {
 
 
 void save_datas() {
-    if (free_command) {
-        FREE(command);
-        free_command = 0;
-    }
-    if (free_light_color) {
-        FREE(light_color);
-        free_light_color = 0;
-    }
-    style_dir         = gtk_entry_get_text(GTK_ENTRY(wid_styledir));
-    style_name        = gtk_entry_get_text(GTK_ENTRY(wid_stylename));
-    light_color       = gtk_entry_get_text(GTK_ENTRY(wid_color));
-    command           = gtk_entry_get_text(GTK_ENTRY(wid_command));
-    msgcmd            = gtk_entry_get_text(GTK_ENTRY(wid_msgcmd));
+    FREE(style_dir);
+    FREE(style_name);
+    FREE(light_color);
+    FREE(command);
+    FREE(msgcmd);
+
+    style_dir         = xstrdup(gtk_entry_get_text(GTK_ENTRY(wid_styledir)));
+    style_name        = xstrdup(gtk_entry_get_text(GTK_ENTRY(wid_stylename)));
+    light_color       = xstrdup(gtk_entry_get_text(GTK_ENTRY(wid_color)));
+    command           = xstrdup(gtk_entry_get_text(GTK_ENTRY(wid_command)));
+    msgcmd            = xstrdup(gtk_entry_get_text(GTK_ENTRY(wid_msgcmd)));
     backlight         = GTK_TOGGLE_BUTTON(wid_backlight)->active;
     switch_authorized = GTK_TOGGLE_BUTTON(wid_blink)->active;
     h12               = GTK_TOGGLE_BUTTON(wid_h12)->active;

wmclockmon/wmclockmon-config/tools.c
@@ -17,8 +17,6 @@
 #include <sys/types.h>
 #include "configfile.h"
 
-int    free_light_color;
-int    free_command;
 int    time_mode;
 char  *msgcmd;
 
@@ -119,7 +117,6 @@ void load_cfgfile() {
             printf("Unable to open configuration file \"%s\".\n", config_file);
         return;
     }
-    free_light_color = free_command = 0;
     while (! feof(file)) {
         memset(line, 0, MAXSTRLEN + 1);
         fgets(line, MAXSTRLEN, file);
@@ -132,15 +129,13 @@ void load_cfgfile() {
 
         if (strncmp(line, "Backlight", 9) == 0)
             backlight = getbool(value);
-        else if (strncmp(line, "Color", 5) == 0) {
+        else if (strncmp(line, "Color", 5) == 0)
             light_color = xstrdup(value);
-            free_light_color = 1;
-        } else if (strncmp(line, "Alarm", 5) == 0)
+        else if (strncmp(line, "Alarm", 5) == 0)
             alrm_add(&alarms, value);
-        else if (strncmp(line, "Command", 7) == 0) {
+        else if (strncmp(line, "Command", 7) == 0)
             command = xstrdup(value);
-            free_command = 1;
-        } else if (strncmp(line, "MessageCmd", 10) == 0)
+        else if (strncmp(line, "MessageCmd", 10) == 0)
             msgcmd = xstrdup(value);
         else if (strncmp(line, "Blink", 5) == 0)
             switch_authorized = getbool(value);